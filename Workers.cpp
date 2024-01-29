27e8597b966ace8ec47ac7f1d6ccb825037f3f6a
henritsa <henritsa@stud.ntnu.no>
2024-01-29 13:25:57 +0100

    Worker class executes provided functions

--- Workers.cpp
+++ Workers.cpp
@@ -1,37 +1,38 @@
 //
 // Created by henri on 29.01.2024.
 //
-
 #include "Workers.h"
-# include <functional>
-# include <iostream>
-# include <list>
-# include <mutex>
-# include <thread>
-# include <vector>
+
+using namespace std;
 
 int main() {
+    Workers worker_threads(4);
+    Workers event_loop(1);
+    worker_threads.start(); // Create 4 internal threads
+    event_loop.start(); // Create 1 internal thread
+    worker_threads.post([] {
+        // Task A
+        cout << "Goddag fra Task A \n" << endl;
+    });
+    worker_threads.post([] {
+        // Task B
+        // Might run in parallel with task A
+        cout << "Goddag fra Task B \n" << endl;
+    });
+    event_loop.post([] {
+        // Task C
+        // Might run in parallel with task A and B
+        cout << "Goddag fra Task C \n" << endl;
+    });
+    event_loop.post([] {
+        // Task D
+        // Will run after task C
+        // Might run in parallel with task A and B
+        cout << "Goddag fra Task D \n" << endl;
+    });
+    worker_threads.join(); // Calls join() on the worker threads
+    event_loop.join(); // Calls join() on the event thread
+
+    return 0;
 }
 
-Workers worker_threads(4);
-Workers event_loop(1);
-worker_threads.start(); // Create 4 internal threads
-event_loop.start(); // Create 1 internal thread
-worker_threads.post([] {
-// Task A
-});
-worker_threads.post([] {
-// Task B
-// Might run in parallel with task A
-});
-event_loop.post([] {
-// Task C
-// Might run in parallel with task A and B
-});
-event_loop.post([] {
-// Task D
-// Will run after task C
-// Might run in parallel with task A and B
-});
-worker_threads.join(); // Calls join() on the worker threads
-event_loop.join(); // Calls join() on the event thread
--- Workers.h
+++ Workers.h
@@ -4,12 +4,15 @@
 
 #ifndef EVENT_LOOP_THREADING_WORKERS_H
 #define EVENT_LOOP_THREADING_WORKERS_H
-# include <functional>
-# include <iostream>
-# include <mutex>
-# include <thread>
-# include <vector>
-
+#include <functional>
+#include <iostream>
+#include <list>
+#include <mutex>
+#include <thread>
+#include <vector>
+#include <queue>
+#include <condition_variable>
+#include "chrono"
 //
 // Workers klassen skal ha en intern variabel? som er en liste med threads
 // konstruktøren skal bestemme hvor mange threads som skal brukes
@@ -22,16 +25,73 @@
 class Workers {
 
 public:
-    Workers(int num)
+    explicit Workers(int num)
         : num_threads(num)
     {}
-    void start() {
 
+    ~Workers() {
+        std::cout<<"destructor"<<std::endl;
+        stop();
+    };
+    void start() {
+        threads.reserve(num_threads);
+        for (int i = 0; i < num_threads; i++) {
+            threads.emplace_back(&Workers::work, this);
+        }
     }
 
+    void post(const std::function<void()>& task) {
+        {
+            std::unique_lock<std::mutex> lock(tasks_mutex);
+            tasks.emplace_back(task);
+        }
+        cv.notify_one();
+    }
+
+    void post_timeout(const std::function<void()>& task, int delay) {
+        std::unique_lock<std::mutex> lock(tasks_mutex);
+        std::this_thread::sleep_for(std::chrono::seconds(delay));
+        tasks.emplace_back(task);
+        cv.notify_one();
+    }
+
+    void join() {
+        for (auto &thread : threads) if (thread.joinable())thread.join();
+    }
+
+
 private:
-    std::vector<thread> threads;
+    std::vector<std::thread> threads;
+    std::list<std::function<void()>> tasks;
     int num_threads;
+    std::mutex tasks_mutex;
+    std::condition_variable cv;
+    bool stopThreads = false;
+
+    void stop() {
+        stopThreads = true;
+        cv.notify_all();
+        join();
+    }
+
+    void work() {
+        while(true) {
+            std::function<void()> task;
+            {
+                std::unique_lock<std::mutex> lock(tasks_mutex);
+                cv.wait(lock, [this] {
+                    return !tasks.empty() || stopThreads;
+                });
+                if (stopThreads) break;
+                if (!tasks.empty()) {
+                    task = *tasks.begin(); // Copy task for later use
+                    tasks.pop_front(); // Remove task from list
+                }
+            }
+            if (task)
+                task();
+        }
+    }
 };
 
 
