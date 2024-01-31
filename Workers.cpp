96384e805b2aa75b2e72ae872e553b57e1ef0ef4
heikkkk <henrik.teksle.sandok@hotmail.no>
2024-01-31 08:41:55 +0100

    Refactor header- and main-file

--- Workers.cpp
+++ Workers.cpp
@@ -5,8 +5,63 @@
 
 using namespace std;
 
+void Workers::start() {
+    threads.reserve(num_threads);
+    for (int i = 0; i < num_threads; i++) {
+        threads.emplace_back(&Workers::work, this);
+    }
+}
+
+void Workers::post( const std::function<void()>& task) {
+    {
+        std::unique_lock<std::mutex> lock(tasks_mutex);
+        tasks.emplace_back(task);
+        cv.notify_one();
+    }
+}
+
+void Workers::post_timeout(const std::function<void()>& task, int delay) {
+    std::unique_lock<std::mutex> lock(tasks_mutex);
+    std::this_thread::sleep_for(std::chrono::seconds(delay));
+    tasks.emplace_back(task);
+    cv.notify_one();
+}
+
+void Workers::join() {
+    {
+        std::unique_lock<std::mutex> lock(tasks_mutex);
+        stop_threads = true;
+        cv.notify_all();
+    }
+    for (auto& thread : threads) thread.join();
+}
+
+bool Workers::stop() {
+    return (stop_threads && tasks.empty());
+}
+
+void Workers::work() {
+    while(!stop()) {
+        std::function<void()> task;
+        {
+            std::unique_lock<std::mutex> lock(tasks_mutex);
+            cv.wait(lock, [this] {
+                return !tasks.empty() || stop_threads;
+            });
+            if (stop()) break;
+            if (!tasks.empty()) {
+                task = *tasks.begin(); // Copy task for later use
+                tasks.pop_front(); // Remove task from list
+            }
+        }
+        if (task)
+            task();
+    }
+}
+
+
 int main() {
-    Workers worker_threads(4);
+    Workers worker_threads(2);
     Workers event_loop(1);
     worker_threads.start(); // Create 4 internal threads
     event_loop.start(); // Create 1 internal thread
@@ -32,6 +87,7 @@ int main() {
     });
     worker_threads.join(); // Calls join() on the worker threads
     event_loop.join(); // Calls join() on the event thread
+
 
     return 0;
 }
--- Workers.h
+++ Workers.h
@@ -1,7 +1,6 @@
 //
 // Created by henri on 29.01.2024.
 //
-
 #ifndef EVENT_LOOP_THREADING_WORKERS_H
 #define EVENT_LOOP_THREADING_WORKERS_H
 #include <functional>
@@ -13,15 +12,7 @@
 #include <queue>
 #include <condition_variable>
 #include "chrono"
-//
-// Workers klassen skal ha en intern variabel? som er en liste med threads
-// konstruktøren skal bestemme hvor mange threads som skal brukes
-// metoden start() skal lage thread-ene i worker_threads? på en eller annen måte skal man kunnne kjøre dette
-// metoden event_loop skal gjøre noe tilsvarende?
-// Metoden post(function<void> f) skal ta inn en funksjon og poste den til thread-listen. denne skal da utføres av en
-// worker thread
-//
-//
+
 class Workers {
 
 public:
@@ -29,35 +20,15 @@ public:
         : num_threads(num)
     {}
 
-    ~Workers() {
-        std::cout<<"destructor"<<std::endl;
-        stop();
-    };
-    void start() {
-        threads.reserve(num_threads);
-        for (int i = 0; i < num_threads; i++) {
-            threads.emplace_back(&Workers::work, this);
-        }
-    }
 
-    void post(const std::function<void()>& task) {
-        {
-            std::unique_lock<std::mutex> lock(tasks_mutex);
-            tasks.emplace_back(task);
-        }
-        cv.notify_one();
-    }
 
-    void post_timeout(const std::function<void()>& task, int delay) {
-        std::unique_lock<std::mutex> lock(tasks_mutex);
-        std::this_thread::sleep_for(std::chrono::seconds(delay));
-        tasks.emplace_back(task);
-        cv.notify_one();
-    }
+    void start();
 
-    void join() {
-        for (auto &thread : threads) if (thread.joinable())thread.join();
-    }
+    void post( const std::function<void()>& task);
+
+    void post_timeout(const std::function<void()>& task, int delay);
+
+    void join();
 
 
 private:
@@ -66,32 +37,11 @@ private:
     int num_threads;
     std::mutex tasks_mutex;
     std::condition_variable cv;
-    bool stopThreads = false;
+    bool stop_threads = false;
 
-    void stop() {
-        stopThreads = true;
-        cv.notify_all();
-        join();
-    }
+    bool stop();
 
-    void work() {
-        while(true) {
-            std::function<void()> task;
-            {
-                std::unique_lock<std::mutex> lock(tasks_mutex);
-                cv.wait(lock, [this] {
-                    return !tasks.empty() || stopThreads;
-                });
-                if (stopThreads) break;
-                if (!tasks.empty()) {
-                    task = *tasks.begin(); // Copy task for later use
-                    tasks.pop_front(); // Remove task from list
-                }
-            }
-            if (task)
-                task();
-        }
-    }
+    void work();
 };
 
 
